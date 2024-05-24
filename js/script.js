5b554ae6fdb8b8b1eea3d085a9963346c8669161
mauro52 <maurobulacio@gmail.com>
2024-05-23 21:31:55 -0300

    Difficulty done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-//Used for developer view / consolelog
-let devParam = "Testing";
 //Game Difficulty defaults
 let gameDif = "Easy";
 let dungeonRooms = 3;
@@ -34,11 +32,6 @@ const roomsDescriptions = {
     roomDesc: ["Antorchas enclavadas en las paredes de piedra proyectan sombras titilantes sobre el suelo polvoriento lleno de tripas. Una pesada puerta de hierro está entreabierta, sus bisagras crujen suavemente en el aire rancio.", "Sarcófagos de piedra descansan en nichos a lo largo de las paredes, sus tapas adornadas con runas descoloridas. El aire está cargado con el olor a descomposición, y telarañas se aferran al techo como velos espectrales.", "Estanterías llenas de frascos y tarros polvorientos alinean las paredes, cada uno conteniendo sustancias misteriosas de varios colores. Un caldero burbujeante se encuentra sobre un fuego crepitante, enviando volutas de humo coloreado al aire.", "Montones de relucientes monedas de oro y gemas preciosas brillan en la luz tenue, amontonadas sobre pedestales de piedra antiguos. Artefactos engastados con joyas y armas ornamentadas se exhiben en estantes forrados de terciopelo, sus superficies pulidas hasta obtener un brillo intenso.", "Barras de hierro dividen las cámaras angostas, cada una con una cama oxidada y un suelo cubierto de paja. Ecos tenues de gemidos distantes y cadenas que golpean llenan el silencio opresivo, otorgando una atmósfera inquietante al aire frío y húmedo.", "Una estatua grandiosa de piedra tallada domina la cámara, flanqueado por otros adornos imponentes de reyes olvidados. Banderas desgarradas cuelgan de las paredes, sus colores descoloridos llevan los sigilos de casas nobles hace mucho extintas."]
 }
 
-//show variable on console
-function DeveloperView(devParam) {
-    console.log(devParam);
-}
-
 let playerNameButton = document.getElementById("playerNamebutton");
 playerNameButton.addEventListener("click", function () { getPlayerName() });
 
@@ -47,31 +40,22 @@ function getPlayerName() {
 
     if (playerStats.name === null || playerStats.name === "") {
         return;
-        /*document.getElementById("spanName").innerHTML = "invalido";
-        document.getElementById("spanName").style.color = "red";
-        let names = ["Blade", "Lego", "Mah"];
-        min = 0;
-        max = names.length - 1;
-        playerStats.name = names[Math.floor(Math.random() * (max - min + 1)) + min];
-        */
     }
     document.getElementById("spanName").style.color = "red";
     document.getElementById("spanName").innerHTML = playerStats.name;
 }
 
-//let radios = document.getElementsByClassName("divDif")
-//console.log(radios[0].children);
-
-let input = document.getElementById("difHard").ariaChecked;
-console.log(input);
+let difButton = document.getElementById("difButton");
+difButton.addEventListener("click", function () { gameDifficulty() });
 
 function gameDifficulty() {
-
-    if (document.getElementById("difEasy").ariaChecked) {
+    if (document.getElementById("difEasy").checked) {
         console.log("oeasy");
-    } else if (document.getElementById("difHard").ariaChecked) {
-        console.log("hard");
+        gameDif = "Easy";
+    } else if (document.getElementById("difHard").checked) {
+        gameDif = "Hard";;
     }
+    startGame();
 }
 
 
@@ -152,6 +136,6 @@ function treasure() {
 }
 
 //getPlayerName();
-gameDifficulty();
+//gameDifficulty();
 //startGame();
 //exploration();
